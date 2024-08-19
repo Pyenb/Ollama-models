04076f79c508be64f996e309ede50c6efe2a7574
Pyenb <py@pyenb.network>
2024-08-19 15:51:06 +0200

    update table gen

--- gen-table.py
+++ gen-table.py
@@ -12,21 +12,28 @@ html_content = response.text
 soup = BeautifulSoup(html_content, 'html.parser')
 table = soup.find('table')
 rows = table.find_all('tr')[2:]
-markdown_table = "| Model Name | Last Modified | Size | Download Link |\n| --- | --- | --- | --- |\n"
-
+markdown_table = "| Model | Parameters | Last Modified | Size | Download Link |\n| --- | --- | --- | --- | --- |\n"
 for row in rows:
     columns = row.find_all('td')
     if len(columns) < 4:
         continue
-    file_name = columns[1].find('a').get_text()
+    file_name = columns[1].find('a').get_text().strip()
+    
     if file_name == "Parent Directory":
         continue
     last_modified = columns[2].get_text().strip()
     size = columns[3].get_text().strip()
+    
     file_url = URL + file_name
     file_name = file_name.replace('.tar.gz', '')
     
-    markdown_table += f"| {file_name} | {last_modified} | {size} | [Download]({file_url}) |\n"
+    if ':' in file_name:
+        model, parameters = file_name.split(':', 1)
+    else:
+        model = file_name
+        parameters = '---'
+    
+    markdown_table += f"| {model} | {parameters} | {last_modified} | {size} | [Download]({file_url}) |\n"
 
 with open("README.md", "r", encoding="utf8") as file:
     readme = file.read()
